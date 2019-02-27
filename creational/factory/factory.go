5d74bb8d2286b3b9e4bd8a345f391b5ea52c365a
Leonardo Souza <leogsouza@gmail.com>
2019-02-27 15:27:58 -0300

    Replace DebitCard by CreditCard

--- creational/factory/factory.go
+++ creational/factory/factory.go
@@ -44,11 +44,6 @@ func (c *CashPM) Pay(amount float32) string {
 	return fmt.Sprintf("%0.2f paid using cash\n", amount)
 }
 
-// Pay implements payment method for DebitCardPM
-func (c *DebitCardPM) Pay(amount float32) string {
-	return fmt.Sprintf("%#0.2f paid using debit card\n", amount)
-}
-
 // Pay implements payment method for CreditCardPM
 func (d *CreditCardPM) Pay(amount float32) string {
 	return fmt.Sprintf("%#0.2f paid using debit card (new)\n",
