61acef834c874d69b7670b3ed20a50a9c2afa6f8
Leonardo Souza <leogsouza@gmail.com>
2019-02-28 18:04:25 -0300

    Abstract Factory - Add some units tests cases

--- creational/abstractfactory/vehicle_factory_test.go
+++ creational/abstractfactory/vehicle_factory_test.go
@@ -38,6 +38,7 @@ func TestCarFactory(t *testing.T) {
 	}
 
 	t.Logf("Car vehicle has %d wheels\n", carVehicle.NumWheels())
+	t.Logf("Car vehicle has %d seats\n", carVehicle.NumSeats())
 
 	luxuryCar, ok := carVehicle.(Car)
 
@@ -45,4 +46,33 @@ func TestCarFactory(t *testing.T) {
 		t.Fatal("Struct assertion has failed")
 	}
 	t.Logf("Luxury car has %d doors.\n", luxuryCar.NumDoors())
+
+	carVehicle, err = carF.Build(FamilyCarType)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	familyCar, ok := carVehicle.(Car)
+
+	if !ok {
+		t.Fatal("Struct assertion has falied")
+	}
+
+	t.Logf("Family car has %d doors", familyCar.NumDoors())
+	t.Logf("Family car has %d seats\n", carVehicle.NumSeats())
+
+	_, err = carF.Build(20)
+	if err == nil {
+		t.Error("A car type with ID 20 must return an error")
+	}
+	t.Log("LOG:", err)
+}
+
+func TestCarNonExistent(t *testing.T) {
+	_, err := BuildFactory(10)
+
+	if err == nil {
+		t.Error("A Factory type with ID 10 must return an error")
+	}
+	t.Log("Log:", err)
 }
