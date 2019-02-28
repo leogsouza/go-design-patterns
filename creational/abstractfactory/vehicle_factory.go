094ad83c2846da857d1f33a687e8549a1e8e3041
Leonardo Souza <leogsouza@gmail.com>
2019-02-28 14:40:44 -0300

    Abstract Factory - Adjust VehicleFactory

--- creational/abstractfactory/vehicle_factory.go
+++ creational/abstractfactory/vehicle_factory.go
@@ -1,7 +1,23 @@
 package abstractfactory
 
+import "fmt"
+
 // VehicleFactory is the interface by an object that creates
 // a new Vehicle
 type VehicleFactory interface {
-	NewVehicle(v int) (Vehicle, error)
+	Build(v int) (Vehicle, error)
+}
+
+// Factory types
+const (
+	CarFactoryType       = 1
+	MotorbikeFactoryType = 2
+)
+
+// BuildFactory create a new factory according the type
+func BuildFactory(f int) (VehicleFactory, error) {
+	switch f {
+	default:
+		return nil, fmt.Errorf("factory with id %d not recognized", f)
+	}
 }
