f6e5e5749c5504dd0c3284cccddbb50018cbb711
Leonardo Souza <leogsouza@gmail.com>
2019-03-01 16:40:56 -0300

    Prototype - Create structure

---
+++ creational/prototype/go.mod
@@ -0,0 +1,1 @@
+module github.com/leogsouza/go-design-patterns/creational/prototype
---
+++ creational/prototype/prototype.go
@@ -0,0 +1,51 @@
+package prototype
+
+import (
+	"errors"
+)
+
+type ShirtCloner interface {
+	GetClone(s int) (ItemInfoGetter, error)
+}
+
+const (
+	White = 1
+	Black = 2
+	Blue  = 3
+)
+
+func GetShirtsCloner() ShirtCloner {
+	return nil
+}
+
+type ShirtsCache struct{}
+
+func (s *ShirtsCache) GetClone(m int) (ItemInfoGetter, error) {
+	return nil, errors.New("Not implemented yet")
+}
+
+type ItemInfoGetter interface {
+	GetInfo() string
+}
+
+type ShirtColor byte
+
+type Shirt struct {
+	Price float32
+	SKU   string
+	Color ShirtColor
+}
+
+func (s *Shirt) GetInfo() string {
+	return ""
+}
+
+var whitePrototype *Shirt = &Shirt{
+	Price: 15.00,
+	SKU:   "empty",
+	Color: White,
+}
+
+func (i *Shirt) GetPrice() float32 {
+	return i.Price
+}
