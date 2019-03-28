e74a2a6c2c25bc96326af455ff7d07da512b3eb4
Leonardo Souza <leogsouza@gmail.com>
2019-03-28 00:14:10 -0300

    Adapter - Add Implementation and Unit tests

---
+++ structural/adapter/adapter.go
@@ -0,0 +1,41 @@
+package adapter
+
+import "fmt"
+
+// LegacyPrints is an old interface
+type LegacyPrinter interface {
+	Print(s string) string
+}
+
+// MyLegacyPrinter implements LegacyPrinter
+type MyLegacyPrinter struct{}
+
+// Print accepts a string and return a message
+func (l *MyLegacyPrinter) Print(s string) (newMsg string) {
+	newMsg = fmt.Sprintf("Legacy Printer: %s\n", s)
+	fmt.Println(newMsg)
+	return
+}
+
+// ModernPrinter is an new interface
+type ModernPrinter interface {
+	PrintStored() string
+}
+
+// PrinterAdapter works as an adapter from
+// old interface to new interface
+type PrinterAdapter struct {
+	OldPrinter LegacyPrinter
+	Msg        string
+}
+
+// PrinStored
+func (p *PrinterAdapter) PrintStored() (newMsg string) {
+	if p.OldPrinter != nil {
+		newMsg = fmt.Sprintf("Adapter: %s", p.Msg)
+		newMsg = p.OldPrinter.Print(newMsg)
+	} else {
+		newMsg = p.Msg
+	}
+	return
+}
