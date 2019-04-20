0a384a2561f7eb15efffc6d218efbf174786428d
Leonardo Souza <leogsouza@gmail.com>
2019-04-20 13:06:07 -0300

    Flyweight - Create the structure

---
+++ structural/flyweight/flyweight.go
@@ -0,0 +1,52 @@
+package flyweight
+
+import (
+	"time"
+)
+
+const (
+	TEAM_A = iota
+	TEAM_B
+)
+
+type Team struct {
+	ID             uint64
+	Name           string
+	Shield         []byte
+	Players        []Player
+	HistoricalData []HistoricalData
+}
+
+type Player struct {
+	Name         string
+	Surname      string
+	PreviousTeam uint64
+	Photo        []byte
+}
+
+type HistoricalData struct {
+	Year          uint8
+	LeagueResults []Match
+}
+
+type Match struct {
+	Date          time.Time
+	VisitorID     uint64
+	LocalID       uint64
+	LocalScore    byte
+	VisitorScore  byte
+	LocalShoots   uint16
+	VisitorShoots uint16
+}
+
+type teamFlyweightFactory struct {
+	createdTeams map[string]*Team
+}
+
+func (t *teamFlyweightFactory) GetTeam(name string) *Team {
+	return nil
+}
+
+func (t *teamFlyweightFactory) GetNumberOfObject() int {
+	return 0
+}
