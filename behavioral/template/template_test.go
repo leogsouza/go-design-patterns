b65409006c23f0987211d9f5e1874c13cbd4fa3f
Leonardo Souza <leogsouza@gmail.com>
2019-05-01 23:11:37 -0300

    Template - Example using anonymous functions

--- behavioral/template/template_test.go
+++ behavioral/template/template_test.go
@@ -17,11 +17,22 @@ func TestTemplate_ExecuteAlgorithm(t *testing.T) {
 	t.Run("Using interfaces", func(t *testing.T) {
 		s := &TestStruct{Templater: &Template{}}
 		res := s.ExecuteAlgorithm(s)
-		expected := "world"
 
-		if !strings.Contains(res, expected) {
-			t.Errorf("Expected string '%s' wasn't found on retuerned string\n",
-				expected)
-		}
+		expectedOrError(res, " world ", t)
 	})
+
+	t.Run("Using anonymous functions", func(t *testing.T) {
+		m := new(AnonymousTemplate)
+		res := m.ExecuteAlgorithm(func() string {
+			return "world"
+		})
+		expectedOrError(res, " world ", t)
+	})
+}
+
+func expectedOrError(res string, expected string, t *testing.T) {
+	if !strings.Contains(res, expected) {
+		t.Errorf("Expected string '%s' was not found on returned string\n",
+			expected)
+	}
 }
