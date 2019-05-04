740f38e62078d3cbe6dd2b5ee4ea661571b81288
Leonardo Souza <leogsouza@gmail.com>
2019-05-03 22:57:55 -0300

    Interpreter - Add implementation

--- behavioral/interpreter/interpreter.go
+++ behavioral/interpreter/interpreter.go
@@ -1,9 +1,80 @@
 package interpreter
 
 import (
-	"fmt"
+	"strconv"
+	"strings"
 )
 
+// constants for calculator's operations
+const (
+	SUM = "sum"
+	SUB = "sub"
+	MUL = "mul"
+	DIV = "div"
+)
+
+// Calculate calculates the operations passed by parameter
 func Calculate(o string) (int, error) {
-	return 0, fmt.Errorf("Not implemented yet")
+	stack := polishNotationStack{}
+	operators := strings.Split(o, " ")
+
+	for _, operatorString := range operators {
+		if isOperator(operatorString) {
+			right := stack.Pop()
+			left := stack.Pop()
+			mathFunc := getOperationFunc(operatorString)
+			res := mathFunc(left, right)
+			stack.Push(res)
+		} else {
+			val, err := strconv.Atoi(operatorString)
+			if err != nil {
+				return 0, err
+			}
+			stack.Push(val)
+		}
+	}
+	return int(stack.Pop()), nil
+}
+
+type polishNotationStack []int
+
+func (p *polishNotationStack) Push(s int) {
+	*p = append(*p, s)
+}
+
+func (p *polishNotationStack) Pop() int {
+	length := len(*p)
+
+	if length > 0 {
+		temp := (*p)[length-1]
+		*p = (*p)[:length-1]
+		return temp
+	}
+	return 0
+}
+
+func isOperator(o string) bool {
+	return o == SUM || o == SUB || o == MUL || o == DIV
+}
+
+func getOperationFunc(o string) func(a, b int) int {
+	switch o {
+	case SUM:
+		return func(a, b int) int {
+			return a + b
+		}
+	case SUB:
+		return func(a, b int) int {
+			return a - b
+		}
+	case MUL:
+		return func(a, b int) int {
+			return a * b
+		}
+	case DIV:
+		return func(a, b int) int {
+			return a / b
+		}
+	}
+	return nil
 }
