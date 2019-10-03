fb4f0ab1b4fb19ffda2ecd485ee78e5d42e7b4de
Leonardo Souza <leogsouza@gmail.com>
2019-10-03 14:59:03 -0300

    Concurrency Future - Add implementation

--- concurrency/future/future.go
+++ concurrency/future/future.go
@@ -1,20 +1,40 @@
 package future
 
+// SuccessFunc is a type function to be used on success call future
 type SuccessFunc func(string)
+
+// FailFunc is a type function to be used on fail
 type FailFunc func(error)
+
+//ExecuteStringFunc is a type function to be used on execution
 type ExecuteStringFunc func() (string, error)
 
+// MaybeString is a type to use the future pattern
 type MaybeString struct {
+	successFunc SuccessFunc
+	failFunc    FailFunc
 }
 
+// Success is called when future is ok
 func (s *MaybeString) Success(f SuccessFunc) *MaybeString {
-	return nil
+	s.successFunc = f
+	return s
 }
 
+// Fail is called when future failed
 func (s *MaybeString) Fail(f FailFunc) *MaybeString {
-	return nil
+	s.failFunc = f
+	return s
 }
 
+// Execute executes the function ExecuteStringFunc
 func (s *MaybeString) Execute(f ExecuteStringFunc) {
-
+	go func(s *MaybeString) {
+		str, err := f()
+		if err != nil {
+			s.failFunc(err)
+		} else {
+			s.successFunc(str)
+		}
+	}(s)
 }
